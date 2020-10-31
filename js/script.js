e41d2b8608a5874ff8ef7ab9f4dbc4730b6a3170
YY161 <161scope@gmail.com>
2020-10-31 22:28:49 +0900

    サイトの反映

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function() {
+  $('a[href^="#"]').click(function() {
+    var speed = 400;
+		var offset = 100;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - offset;
+    $('body,html').animate({scrollTop: position}, speed, 'swing');
+    return false;
+  });
+
+  $(window).scroll(function() {
+    $('.fadein').each(function() {
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + windowHeight / 2 && !$(this).hasClass("active")) {
+        $(this).addClass("active");
+      }
+    });
+  });
+});
